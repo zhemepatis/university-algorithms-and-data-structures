16749f1603f847dcbf68ef08fb88b6c8d83535da
zhemepatis <rink.gab@gmail.com>
2023-04-28 08:48:05 +0300

    made tripc costs array dynamic

--- ex4/main.cpp
+++ ex4/main.cpp
@@ -7,8 +7,11 @@
 using namespace std;
 
 void printErr(string msg);
+void printArr(double **arr, int arrSize);
 
 int getElementIndex(vector <string> vector, string element);
+void reallocArr(double ***arr, int newSize);
+void freeArr(double ***arr, int size);
 
 int main(int argc, char **argv) {
     if(argc != 3) {
@@ -36,7 +39,7 @@ int main(int argc, char **argv) {
     }
 
     vector <string> cityList;
-    double tripCosts[100][100] = {0};
+    double **tripCosts = new double*;
     while(!fd.eof()) {
         string firstCity, secondCity;
         double cost;
@@ -47,34 +50,42 @@ int main(int argc, char **argv) {
         if(firstCityIndex == -1) {
             firstCityIndex = cityList.size();
             cityList.push_back(firstCity);
+
+            reallocArr(&tripCosts, cityList.size());
         } 
 
         int secondCityIndex = getElementIndex(cityList, secondCity);
         if(secondCityIndex == -1) {
             secondCityIndex = cityList.size();
             cityList.push_back(secondCity);
+
+            reallocArr(&tripCosts, cityList.size());
         }
 
         tripCosts[firstCityIndex][secondCityIndex] = cost;
         tripCosts[secondCityIndex][firstCityIndex] = cost;
     }
+    // printArr(tripCosts, cityList.size());
 
-    int cityNum = cityList.size();
-    for(int i = 0; i < cityNum; ++i) {
-        for(int j = 0; j < cityNum; ++j) {
-            cout << tripCosts[i][j] << " ";
-        }
-        cout << endl;
-    }
     
-
-
+    freeArr(&tripCosts, cityList.size());
 
     return 0;
 }
 
 void printErr(string msg) {
     cout << "Error: " << msg << '\n';
+}
+
+void printArr(double **arr, int arrSize) {
+    cout << "\nPRINTING ARRAY \n";
+
+    for(int i = 0; i < arrSize; ++i) {
+        for(int j = 0; j < arrSize; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 int getElementIndex(vector <string> vector, string element) {
@@ -84,4 +95,18 @@ int getElementIndex(vector <string> vector, string element) {
         return abs(distance(it, vector.begin()));
 
     return -1;
+}
+
+void reallocArr(double ***arr, int newSize) {
+    *arr = (double **) realloc(*arr, newSize*sizeof(double *));
+    for(int i = 0; i < newSize; ++i) {
+        (*arr)[i] = (double *) realloc((*arr)[i], newSize*sizeof(double));
+    }
+}
+
+void freeArr(double ***arr, int size) {
+    for(int i = 0; i < size; ++i) {
+        free((*arr)[i]);
+    }
+    free(*arr);
 }
